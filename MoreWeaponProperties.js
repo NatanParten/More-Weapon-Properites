04ca5256a2474548bda89bd3e6dfd6f2ae201806
NatanParten <136640960+NatanParten@users.noreply.github.com>
2024-01-03 14:19:42 -0500

    Update MoreWeaponProperties.js

--- MoreWeaponProperties.js
+++ MoreWeaponProperties.js
@@ -1,1 +1,13 @@
-
+Hooks.on('ready', () => {
+	CONFIG.DND5E.weaponProperties['cleave'] = 'Cleave';
+  CONFIG.DND5E.weaponProperties['flex'] = 'Flex';
+	CONFIG.DND5E.weaponProperties['graze'] = 'Graze';
+	CONFIG.DND5E.weaponProperties['hail'] = 'Hail';	
+	CONFIG.DND5E.weaponProperties['nick'] = 'Nick';
+	CONFIG.DND5E.weaponProperties['push'] = 'Push';
+	CONFIG.DND5E.weaponProperties['sap'] = 'Sap';
+	CONFIG.DND5E.weaponProperties['slow'] = 'Slow';
+	CONFIG.DND5E.weaponProperties['topple'] = 'Topple';
+	CONFIG.DND5E.weaponProperties['vex'] = 'Vex';
+	
+});
